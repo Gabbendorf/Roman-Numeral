f27472e1c90f6b76b5defaf59c64f9a3fbf37893
Gabba <gmedas@8thlight.com>
2018-03-13 12:53:21 +0000

    Creates a const for arabic and roman numbers outside constructor

--- src/romanNumeral.js
+++ src/romanNumeral.js
@@ -1,27 +1,26 @@
+const converter = new Map([
+  [1000, "M"],
+  [900, "CM"],
+  [500, "D"],
+  [400, "CD"],
+  [100, "C"],
+  [90, "XC"],
+  [50, "L"],
+  [40, "XL"],
+  [10, "X"],
+  [9, "IX"],
+  [5, "V"],
+  [4, "IV"],
+  [1, "I"],
+]);
+
 module.exports = class RomanNumeral {
 
-  constructor() {
-    this.converter = new Map();
-    this.converter.set(1000, "M");
-    this.converter.set(900, "CM");
-    this.converter.set(500, "D");
-    this.converter.set(400, "CD");
-    this.converter.set(100, "C");
-    this.converter.set(90, "XC");
-    this.converter.set(50, "L");
-    this.converter.set(40, "XL");
-    this.converter.set(10, "X");
-    this.converter.set(9, "IX");
-    this.converter.set(5, "V");
-    this.converter.set(4, "IV");
-    this.converter.set(1, "I");
-  }
-
   convert(number) {
-    var convertedNumber = "";
-    for (var arabicNumber of this.converter.keys()) {
+    let convertedNumber = "";
+    for (let arabicNumber of converter.keys()) {
       while (number >= arabicNumber) {
-	convertedNumber += this.converter.get(arabicNumber);
+	convertedNumber += converter.get(arabicNumber);
 	number -= arabicNumber;
       }
     }
